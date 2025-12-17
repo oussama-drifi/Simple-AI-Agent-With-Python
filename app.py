4c467319781dae08c91e192cc36119d03652c64c
oussamadrf <oussamadrifi107@gmail.com>
2025-12-17 23:42:05 +0100

    created flask app

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_cors import CORS
+from api.tasks import register_tasks_routes
+
+def create_app():
+    app = Flask(__name__)
+    CORS(app)  # for React
+    register_tasks_routes(app)
+    return app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
